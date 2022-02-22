1b153b9a6ee7eea68f9f36731a8dfc009a45a36d
Kevin Otieno <oti.kevin@gmail.com>
2022-02-22 23:39:56 +0300

    Init backend + generate Android keystore

---
+++ backend/src/test/java/com/elmenture/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmenture.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
