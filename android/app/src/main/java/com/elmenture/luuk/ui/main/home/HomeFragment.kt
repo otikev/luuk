2bc0df40391de55f06d3370b0599006debf8db16
Kevin Otieno <oti.kevin@gmail.com>
2022-04-26 23:04:14 +0300

    Display item tags in Item detail screen

--- android/app/src/main/java/com/elmenture/luuk/ui/main/home/HomeFragment.kt
+++ android/app/src/main/java/com/elmenture/luuk/ui/main/home/HomeFragment.kt
@@ -287,7 +287,8 @@ class HomeFragment : BaseFragment(), CardStackListener {
                         sizeInternational = item.sizeInternational,
                         sizeNumber = item.sizeNumber,
                         itemId = item.id!!,
-                        description = item.description!!
+                        description = item.description!!,
+                        tagProperties =  item.tagProperties!!
                     )
                 )
             }
--- android/common/src/main/java/models/Spot.kt
+++ android/common/src/main/java/models/Spot.kt
@@ -8,7 +8,8 @@ data class Spot(
     val sizeInternational: String?,
     val sizeNumber: Int?,
     val sizeType: String,
-    val itemId: Long
+    val itemId: Long,
+    var tagProperties: List<Long>? = null
 ) {
     companion object {
         private var counter = 0L
