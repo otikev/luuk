8423c9c1eb2d277a5bb43aa7502cdc9edf7dc668
Baraza Odhiambo <i.odhiambo@kokonetworks.com>
2022-06-10 15:32:50 +0300

    Canned Searches:Work in progress: return item list instead of IDs

--- backend/src/main/java/com/elmenture/core/service/impl/ItemServiceImpl.java
+++ backend/src/main/java/com/elmenture/core/service/impl/ItemServiceImpl.java
@@ -188,8 +188,7 @@ public class ItemServiceImpl implements ItemService {
     private List<ItemDto> styleWeLove() {
         List<String> recommendation = getRecommendationByDayAndTime();
         List<Long> tagIds = tagPropertyRepository.getTagPropertyIds(recommendation);
-        List<Long> itemIds = itemPropertyRepository.findItemIdsByTagPropertyId(tagIds);
-        return itemRepository.findAllById(itemIds).stream().map(item -> mapToDTO(item)).collect(Collectors.toList());
+        return itemPropertyRepository.findItemIdsByTagPropertyId(tagIds).stream().map(item -> mapToDTO(item)).collect(Collectors.toList());
     }
 
     private List<String> getRecommendationByDayAndTime() {
